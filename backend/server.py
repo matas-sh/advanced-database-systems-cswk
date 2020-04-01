393ab967b68a9fdbb5d6c4d34fe9b3fae4bda9ab
TristanJP <trisperk@hotmail.com>
2020-04-01 23:05:22 +0100

    added sanitisation

--- backend/server.py
+++ backend/server.py
@@ -6,11 +6,50 @@ from bson import json_util
 
 app = Flask(__name__)
 
+# Helpers =================
+
 def bson_to_json_response(bson_data):
     # Create a JSON object from bson Cursor
     json_obj = json.loads(json.dumps(list(bson_data), default=json_util.default))
     json_data = jsonify(json_obj)
     return json_data
+
+def check_parameter_type_and_bounds(required_params, recieved_params, error_dict, parameter_name, required_type, bounds):
+    if parameter_name in recieved_params:
+        value = recieved_params[parameter_name]
+        try:
+            if required_type == "float":
+                value = float(value)
+            elif required_type == "int":
+                value = int(value)
+            else:
+                print(f"Unknown expected type: {required_type}")
+                
+            if value > bounds[0] and value <= bounds[1]:
+                recieved_params[parameter_name] = value
+            else:
+                error_dict["Invalid Request"].setdefault("Out of Bounds Error", list()).append(f"{parameter_name} - Must be ({bounds[0]} < x <= {bounds[1]}) but recieved: {value}")
+        except ValueError:
+            error_dict["Invalid Request"].setdefault("Type Error", list()).append(f"{parameter_name} - Expected type ({required_type}) but recieved: {value}")
+    else:
+        error_dict["Invalid Request"].setdefault("Missing parameter(s)", list()).append(parameter_name)
+
+def get_sanitised_params(required_params):
+    errors = {"Invalid Request": {}}
+
+    # Get parameters
+    recieved_params = dict(request.args)
+
+    # Parameter Checking
+    for parameter_detail in required_params:
+        check_parameter_type_and_bounds(required_params, recieved_params, errors, parameter_detail[0], parameter_detail[1], parameter_detail[2])
+    
+    if errors["Invalid Request"]:
+        return errors
+    else:
+        return recieved_params
+
+# Routes =================
 
 @app.route('/')
 def index():
@@ -18,27 +57,27 @@ def index():
 
 @app.route('/crimes-near-location')
 def crimes_near_location():
-    longitude = request.args.get('longitude', type = float)
-    latitude = request.args.get('latitude', type = float)
-    distance = request.args.get('distance', type = int)
+    required_params = [("longitude", "float", (-180.0, 180.0)), ("latitude", "float", (-90.0, 90.0)), ("distance", "int", (0, 1000))]
+    
+    parameters = get_sanitised_params(required_params)
 
-    if not all(item in request.args for item in ["longitude", "latitude", "distance"]) or (None in [longitude, latitude, distance]):
-        return "Invalid Request"
-    else:
-        query = {
-            "location": {
-                "$near": {
-                "$geometry": {
-                    "type": "Point" ,
-                    "coordinates": [ longitude , latitude ]
-                },
-                "$maxDistance": distance
-                }
+    if "Invalid Request" in parameters:
+        return jsonify(parameters)
+    
+    query = {
+        "location": {
+            "$near": {
+            "$geometry": {
+                "type": "Point" ,
+                "coordinates": [ parameters["longitude"] , parameters["latitude"] ]
+            },
+            "$maxDistance": parameters["distance"]
             }
         }
+    }
 
-        bson_data = crimes_collection.find(query)
-        return bson_to_json_response(bson_data)
+    bson_data = crimes_collection.find(query)
+    return bson_to_json_response(bson_data)
 
 @app.route('/test')
 def test():
