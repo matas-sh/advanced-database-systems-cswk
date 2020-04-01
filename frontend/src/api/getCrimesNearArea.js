153f34debfe8ec7035eb0e362ea50837d1962270
TristanJP <trisperk@hotmail.com>
2020-04-01 23:56:43 +0100

    removed commented code

--- frontend/src/api/getCrimesNearArea.js
+++ frontend/src/api/getCrimesNearArea.js
@@ -1,12 +1,5 @@
 const PORT = 5000;
-// const LOCALHOST = process.env.REACT_APP_API_HOST || '127.0.0.1';
 const HOSTNAME = window.location.hostname;
-// const options = {
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Origin': 'http://192.168.0.82:5000',
-//   },
-// };
 
 export default async function getCrimesNearArea(longtitude, latitude, distance) {
   const response = await fetch(`http://${HOSTNAME}:${PORT}/crimes-near-location?longitude=${longtitude}&latitude=${latitude}&distance=${distance}`);
