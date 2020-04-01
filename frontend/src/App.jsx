40a761c87c24ce003f5dbad0fdb2770d0e8a7d89
mataxx5 <m.bartosevicius@hotmail.co.uk>
2020-04-01 17:18:57 +0100

    added frontend fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.jpg';
 import '../style/app.scss';
+import getCrimesNearArea from '../api/getCrimesNearArea';
 
 function App() {
+
+  getCrimesNearArea(asda, asdasd, 100).then(
+    (data) => console.log(data)
+  );
+
   return (
     <div className="App">
       <header className="App-header">
