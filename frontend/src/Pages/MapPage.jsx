a5f8a54699be94c3302cffa155b9da50b4e4136a
mataxx5 <m.bartosevicius@hotmail.co.uk>
2020-04-02 16:04:57 +0100

    restructured UI

--- frontend/src/Pages/MapPage.jsx
+++ frontend/src/Pages/MapPage.jsx
@@ -4,12 +4,6 @@ import Map from '../components/Map';
 
 
 export default function RenderMap() {
-  // const state = {
-  //   lat: 51.505,
-  //   lng: -0.09,
-  //   zoom: 13,
-  // };
-
   return (
     <div id="app-grid">
       <QueryDrawer />
